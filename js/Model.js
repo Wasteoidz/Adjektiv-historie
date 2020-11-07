5decaa8967356d84f0cd5c8ad47724fa41d93788
Wasteoidz <71650988+Wasteoidz@users.noreply.github.com>
2020-11-07 23:58:02 +0100

    finnished
    potensielle uprades er flere ord i rail og nye historie

--- js/Model.js
+++ js/Model.js
@@ -1,7 +1,8 @@
 // model
 let wrapper = document.getElementById("wrapper");
+let bokWhole = document.getElementById("bookOpen");
 let bookLeftSide = document.getElementById('left');
-let bookRightSide = document.getElementById('right');
+let bookRightSide = document.getElementById("right");
 let bookstate = false;
 let adjectiver = ["hestpeis", "Hestkuk", "Riv kjæft", "På fetthåret", "Ballhest ", "Jævles", "Tykjepelk ", "Fettoter ", "Baillhånka", "Bontjyv ", "Hæsblæsete ", "Håsslæsta ", "Jeggelaktig", "Søvvestpeising ", "Tøvkuk",
     "fævlebikkje ", "fly-mævrær", "Sprettkønn ", "raukvilevippa ", "negativ", "stabil", "tørr", "tøff", "utrygg", "fraværende", "stressende", "stille", "stakkars", "forbauset", "forelsket", "Vaklevoren ",
--- js/View.js
+++ js/View.js
@@ -1,12 +1,13 @@
 // view
 
-updateView();
-
 function updateView() {
     if (bookstate) {
-        wrapper.innerHTML = `
-                                                 <div id="bookOpen">
-                                <div id="left">
+        wrapper.innerHTML = `  
+                                    <div id="background-video">
+                                    <iframe style="width:100vw; height:100vh;" src="https://www.youtube.com/embed/UgHKb_7884o?autoplay=1&controls=0&showinfo=0&autohide=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    </div>
+                                    <div id="bookOpen">
+                                    <div id="left">
                                     Etter en <span id="test" ondrop="drop(event)" ondragover="allowDrop(event)" class="empty" >__________</span> sommer, kommer den <span ondrop="drop(event)" ondragover="allowDrop(event)" class="empty" >__________</span> høsten. Det ser ikke helt lyst ut.
                                     Den <span ondrop="drop(event)" ondragover="allowDrop(event)" class="empty" >__________</span> IS-gruppen blir stadig drøyere, mens selv våre <span ondrop="drop(event)" ondragover="allowDrop(event)" class="empty" >__________</span> svenske brødre er skremt av den <span ondrop="drop(event)" ondragover="allowDrop(event)" class="empty" >__________</span> Putin.
                                     Men det stopper ikke der, listen er <span ondrop="drop(event)" ondragover="allowDrop(event)" class="empty" >__________</span> og lang.
@@ -40,8 +41,8 @@ function updateView() {
                             </div>
                             </div>
                             </div>
-                            <div id="ordKnapp" onclick="updateView()"><img id="bokKnapp" src="bilder/bokKnapp.png"/></div>
-                            <br/> <div id="random" onclick="randomWords(this)"><img id="randomBilde"  src="bilder/terning.png"/></div>
+                            <div id="ordKnapp" onclick="newWords()"><img id="knapp"src="bilder/bokKnapp.png"/></div>
+                            <br/> <div id="random" onclick="randomWords()"><img id="randomBilde"  src="bilder/terning.png"/></div>
                             `;
     } else {
         wrapper.innerHTML = `
@@ -49,4 +50,47 @@ function updateView() {
                                             <img id="bookClosed" onclick="bookOpen();updateView();" src="bilder/bookClosed.png"/>
                                         `;
     }
+}
+
+function randomWords() {
+    console.log("oi");
+    return document.getElementById("bookOpen").innerHTML = `
+                                    <div id="left">
+                                        Etter en ${ spawnWords() } sommer, kommer den ${ spawnWords() } høsten. Det ser ikke helt lyst ut.
+                                        Den ${ spawnWords() } IS-gruppen blir stadig drøyere, mens selv våre ${ spawnWords() } svenske brødre er skremt av den ${ spawnWords() } Putin.
+                                        Men det stopper ikke der, listen er ${ spawnWords() } og lang.
+                                        Nordmenn plager hverandre med ${ spawnWords() } netthets. Den tidligere så ${ spawnWords() } villaksen er i ferd med å forsvinne fra ${ spawnWords() } vassdrag.
+                                        Våre ${ spawnWords() } fotballspillere har sjelden vært dårligere representert i ${ spawnWords() } Premier League, og landets ${ spawnWords() } kornsiloer er ikke lenger trygge.
+                                        Det rakner videre i landets ${ spawnWords() } avishus, mens den ${ spawnWords() } PR-bransjen vokser.
+                                        Ståle ble kastet ut av det ${ spawnWords() } Big Brother-huset, kaffebarutvalget i Oslo blir stadig mer ${ spawnWords() }, og vinteren tør vi ikke engang tenke på. Orker du ikke mer?
+                                        Fatt ${ spawnWords() } mot, det finnes alltid et ${ spawnWords() } botemiddel.
+                                        Fortidens lykkeglimt driver oss fremover når vi vasser i sirup. Din venn i tristessen er den ${ spawnWords() }, men likevel så ${ spawnWords() } nostalgien.
+                                        Så vi går på sopptur i ${ spawnWords() } nikkers og snakker om tidligere tiders ${ spawnWords() } fangstfelter, som dessverre ikke finnes lenger.
+                                        Vi arrangerer pokerkvelder, topper hverandres ${ spawnWords() } fyllehistorier og prater om den legendariske potten som Bønna dro inn i det ${ spawnWords() } kollektivet i studietiden.
+                                        Vi inviterer til middagsselskaper og diskuterer gamle, ${ spawnWords() } venner, gjenopplever ${ spawnWords() } fester, før noen nærmere midnatt setter på ${ spawnWords() } Nik Kershaw-hits, som man danser til med ${ spawnWords() } bevegelser.
+                                        Slikt er likevel ${ spawnWords() } amatørmimring.
+                                        For de som virkelig kan sin nostalgi, drar tilbake til barndommens ${ spawnWords() } bursdager, da lykken var pølse-i-lompe, bløtkake med banan og bringebær, Super-Ted, godteposer med rosinpakker og cola-Bugg – før bursdagsbarnmammaen hysjet på alle, der man samlet seg ${ spawnWords() } og ${ spawnWords() } i ring, og pønsket på ${ spawnWords() } nyord med promp
+                                    </div>          
+                                    <div id="right"> 
+                                        <img id="oluf1" src="bilder/oluf.jpg"/>                                
+                                    </div>
+                            `;
+}
+
+function newWords() {
+  console.log("oi");
+  return document.getElementById("right").innerHTML = `
+                                    <span id="ord1" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord2" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord3" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>   
+                                    <span id="ord4" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord5" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord6" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord7" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord8" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord9" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord10" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord11" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord12" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>   
+                                    <span id="ord13" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord14" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord15" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord16" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord17" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord18" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord19" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord20" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord21" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord22" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord23" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord24" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord25" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord26" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord27" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord28" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord29" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord30" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                    <span id="ord31" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord32" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span><span id="ord33" class="word" ondragstart="drag(event)" draggable="true">${ spawnWords() }</span>
+                                   `;
+
 }
